20437d8f9e1a8c12c69eeb9fcca95a485d7bdad8
Jocatins <jocakhamen@gmail.com>
2020-10-13 11:17:25 +0100

    sphinx-upload-download

---
+++ routes/file.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const _router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+
+const store = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null, './uploads');
+    },
+    filename:function(req,file,cb){
+        cb(null, Date.now()+'.'+file.originalname);
+    }
+});
+
+
+const upload = multer({storage:store}).single('file');
+
+_router.post('/upload', function(req,res,next){
+    upload(req,res,function(err){
+        if(err){
+            return res.status(501).json({error:err});
+        }
+        //do all database record saving activity
+        return res.json({originalname:req.file.originalname, uploadname:req.file.filename});
+    });
+});
+
+
+_router.post('/download', function(req,res,next){
+    filepath = path.join(__dirname,'../uploads') +'/'+ req.body.filename;
+    res.sendFile(filepath);
+});
+
+module.exports = _router;
